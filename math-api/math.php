222cebab65943cbf20c59bf8507a0e0e5db0c7c7
Maxmilián Dao <maxmilian.dao@outlook.com>
2022-12-15 22:30:36 +0100

    Add files via upload

---
+++ math-api/math.php
@@ -0,0 +1,78 @@
+<?php
+    $numbers = explode(",", filter_input(INPUT_GET, "number"));
+    $json = [];
+
+        switch(filter_input(INPUT_GET, "operation"))
+        {
+            case"add";
+                foreach($numbers as $number) {
+                    if (!is_numeric($number)) {
+                        $json = ["REPORT" => "NaN"];
+                        break;
+                        }
+                    else {
+                        $json = ["REPORT" => "OK", "RESULT" => array_sum($numbers)];
+                    }
+                }
+
+            case"sub";
+                foreach($numbers as $number) {
+                    if (!is_numeric($number)) {
+                        $json = ["REPORT" => "NaN"];
+                        break;
+                        }
+                    else {
+                            $result = reset($numbers);
+                            foreach (array_slice($numbers, 1) as $value) {
+                            $result -= $value;
+                            }
+                        $json = ["REPORT" => "OK", "RESULT" => $result];
+                }
+            }
+            case"mul";
+                foreach($numbers as $number) {
+                    if (!is_numeric($number)) {
+                        $json = ["REPORT" => "NaN"];
+                        break;
+                        }
+                else {
+                        $result = reset($numbers);
+                        foreach (array_slice($numbers, 1) as $value) {
+                        $result *= $value;
+                        }
+                        $json = ["REPORT" => "OK", "RESULT" => $result];
+            }
+        }
+            case"div";
+            foreach($numbers as $number) {
+                if (!is_numeric($number)) {
+                    $json = ["REPORT" => "NaN"];
+                    break;
+                    }
+            else {
+                    $result = reset($numbers);
+                    foreach (array_slice($numbers, 1) as $value) {
+                    $result /= $value;
+                    }
+                    $json = ["REPORT" => "OK", "RESULT" => $result];
+        }
+    }
+
+            case"sqrt";
+            foreach($numbers as $number) {
+                if (!is_numeric($number)) {
+                    $json = ["REPORT" => "NaN"];
+                    break;
+                    }
+            else {
+                    $result = reset($numbers);
+                    foreach (array_slice($numbers, 1) as $value) {
+                    $result **= $value;
+                    }
+                    $json = ["REPORT" => "OK", "RESULT" => $result];
+        }
+    }
+
+        }
+            echo json_encode($json);
+?>
