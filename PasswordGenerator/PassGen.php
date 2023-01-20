1b2bbd4f1f1e257e83cadaccd182c2aa22e7ceb9
Maxmilián Dao <maxmilian.dao@outlook.com>
2023-01-20 01:11:14 +0100

    Update PassGen.php
    attemp to fix indexes resulting in undefined variables with if statements setting default values

--- PasswordGenerator/PassGen.php
+++ PasswordGenerator/PassGen.php
@@ -1,27 +1,48 @@
 <?php
     $path = explode("/", $_SERVER['REQUEST_URI']);
     array_splice($path, 1, 1);
-    $length = $path[1];
-    $special = $path[2];
-    $case = $path[3];
-    $numbers = $path[4];
+
+    if (!isset($path[1])) {
+        $length = 8; 
+    } else {
+        $length = $path[1];
+    }
+
+    if (!isset($path[2])) {
+        $special = false; 
+    } else {
+        $special = $path[2];
+    }
+
+    if (!isset($path[3])) {
+        $case = false; 
+    } else {
+        $case = $path[3];
+    }
+
+    if (!isset($path[4])) {
+        $numbers = false; 
+    } else {
+        $numbers = $path[4];
+    }
+
     $password = "";
     $possible_chars = "abcdefghijklmnopqrstuvwxyz";
 
-        if ($special == "true") {
-            $possible_chars .= "!@#€$%^&*{}()_+-=[];':\"<>,.?/\\|`~";
-        }
+    if ($special == "true") {
+        $possible_chars .= "!@#€$%^&*{}()_+-=[];':\"<>,.?/\\|`~";
+    }
 
-        if ($case == "true") {
-            $possible_chars .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        }
+    if ($case == "true") {
+        $possible_chars .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
 
-        if ($case == "true") {
-            $possible_chars .= "0123456789";
-        }
+    if ($numbers == "true") {
+        $possible_chars .= "0123456789";
+    }
 
-        for ($i = 0; $i < $length; $i++) {
-            $password .= $possible_chars[rand(0, strlen($possible_chars) - 1)];
-        }
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $possible_chars[rand(0, strlen($possible_chars) - 1)];
+    }
 
 echo json_encode(["password" => $password]);
